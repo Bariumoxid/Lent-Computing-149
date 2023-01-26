84d57e61f2bac4e74980b102b734b9493d5a43ed
Ragha Rao <ragharao2001@gmail.com>
2023-01-26 00:34:07 +0000

    optional: property

--- floodsystem/station.py
+++ floodsystem/station.py
@@ -41,6 +41,102 @@ class MonitoringStation:
     
     def typical_range_consistent(self):
         return (self.typical_range != None) and (self.typical_range[0] <= self.typical_range[1]) 
+    
+    #==========================================Properties=======================================
+
+    #------------------------------------------Station ID---------------------------------------
+    
+    @property
+    def station_id(self):
+        return self._station_id
+    
+    @station_id.setter
+    def station_id(self,value):
+        try:
+            self._station_id
+            print("NO CHANGES ALLOWED!")
+        except:
+            self._station_id = value
+
+    #------------------------------------------Name-----------------------------------------------
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self,value):
+        try:
+            self._name
+            print("NO CHANGES ALLOWED!")
+        except:
+            self._name = value
+
+    #------------------------------------------Measure_ID-------------------------------------------
+    @property
+    def measure_id(self):
+        return self._measure_id
+    
+    @measure_id.setter
+    def measure_id(self,value):
+        try:
+            self._measure_id
+            print("NO CHANGES ALLOWED!")
+        except:
+            self._measure_id = value
+
+    #------------------------------------------coord------------------------------------------------
+    @property
+    def coord(self):
+        return self._coord
+    
+    @coord.setter
+    def coord(self,value):
+        try:
+            self._coord
+            print("NO CHANGES ALLOWED!")
+        except:
+            self._coord= value
+
+    #------------------------------------------typical_range-----------------------------------------
+    @property
+    def typical_range(self):
+        return self._typical_range
+    
+    @typical_range.setter
+    def typical_range(self,value):
+        try:
+            self._typical_range
+            print("NO CHANGES ALLOWED!")
+        except:
+            self._typical_range = value
+
+    #------------------------------------------river--------------------------------------------------
+    @property
+    def river(self):
+        return self._river
+    
+    @river.setter
+    def river(self,value):
+        try:
+            self._river
+            print("NO CHANGES ALLOWED!")
+        except:
+            self._river = value
+            
+    #------------------------------------------town----------------------------------------------------
+    @property
+    def town(self):
+        return self._town
+    
+    @town.setter
+    def town(self,value):
+        try:
+            self._town
+            print("NO CHANGES ALLOWED!")
+        except:
+            self._town = value
+
+    
 
 def inconsistent_typical_range_stations(stations):
     return [station for station in stations if not station.typical_range_consistent()]
