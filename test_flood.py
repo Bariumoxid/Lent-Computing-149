96433ef599da3d6ad5cb252e2d2115c9de718c46
Bariumoxid <andrewbao0408@gmail.com>
2023-01-31 16:38:17 +0000

    Bug fix

--- test_flood.py
+++ test_flood.py
@@ -1,6 +1,32 @@
 from floodsystem.station import MonitoringStation
 from floodsystem.flood import stations_level_over_threshold,stations_highest_rel_level
 
+f = MonitoringStation(
+                station_id=1,
+                measure_id=1,
+                label='f',
+                coord=(52.141965, 0.148004),
+                typical_range=(1, 2),
+                river='aa',
+                town='aaa')
+
+e = MonitoringStation(
+                station_id=1,
+                measure_id=1,
+                label='e',
+                coord=(51.874767, -1.740083),
+                typical_range=(1, 2),
+                river='aa',
+                town='aaa')
+
+d = MonitoringStation(
+                station_id=1,
+                measure_id=1,
+                label='d',
+                coord=(52.197227, 0.087527),
+                typical_range=(1, 2),
+                river='aa',
+                town='aaa')
 
 c = MonitoringStation(
                 station_id=1,
@@ -37,16 +63,16 @@ def test_stations_level_over_threshold():
 
 
 def test_stations_highest_rel_level():
-    a.latest_level = 2
-    b.latest_level = 3
-    c.latest_level = 2.5
+    d.latest_level = 2
+    e.latest_level = 3
+    f.latest_level = 2.5
     it=1
-    for i in stations_highest_rel_level([a,b,c],3):
+    for i in stations_highest_rel_level([d,e,f],3):
         if it==1:
-            assert i==['b', 2.0]
+            assert i==['e', 2.0]
         elif it==2:
-            assert i==['c', 1.5]
+            assert i==['f', 1.5]
         elif it==3:
-            assert i==['a', 1.0]
+            assert i==['d', 1.0]
         it+=1
 test_stations_highest_rel_level()
